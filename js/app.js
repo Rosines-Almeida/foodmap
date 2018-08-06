81eee401336f656a588f9d2da674f43d9e356863
rosines-almeida <rosines.almeida@gmail.com>
2018-08-05 22:17:28 -0300

    Primeiras alterações

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
 // Add your JavaScript
+
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -23.56261835,
+      lng: -46.65490751
+    },
+    zoom: 8
+  });
+}
+
+/*FUNÇÃO efeito delay  na pagina i */
+$(document).ready(function () {
+  $('.box-2').delay('1000').fadeIn('slow')
+
+
+  /*Função filtrar */
+
+
+  var dataRestaurants = [];
+  var restaurantName = [];
+  var image = [];
+  var types = [];
+  var descriptions = [];
+
+  $.each(restaurantes, function (index, value) {
+    dataRestaurants.push(index);
+    restaurantName.push(value.name);
+    image.push(value.image);
+    types.push(value.type.toUpperCase());
+    descriptions.push(value.description);
+    $("#img-restaurant").append("<img src= " + value.image + " alt=" + value.type.toUpperCase() + " val=" + index + " data-toggle='modal' data-target='#modalRestautante'>");
+
+  })
+
+
+  $('.btn-filtrar').click(function () {
+    var inputValue = $('.user-text').val().toUpperCase();
+    $("img").each(function () {
+      if ($(this).attr("alt") !== inputValue) {
+        $(this).fadeOut('slow');
+      }
+    });
+  })
+
+  $('img').click(function () {
+    var index = $(this).attr("val");
+    $("#name-restaurant").append(restaurantName[index]);
+    $("#type-modal").append(types[index]);
+    $('#description-modal').append(descriptions[index]);
+  });
+});
---
+++ js/data2.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+    'name':'Taco mamalon',
+     'food':'mexicana',
+     'photo':'..assets/images/tacos.png',
+     'adress':'Puebla 205, Roma Nte., 06700 Ciudad de México, CDMX'
+   },
+
+  {'name':'Fonda Fina',
+   'food':'española',
+   'photo':'..assets/images/fonda-fina.png',
+   'adress':'Medellín 79, Roma Nte., 06700 Ciudad de México, CDMX'
+   },
+
+    {'name':'La tecla',
+     'food':'argentina',
+     'photo':'..assets/images/tecla.png',
+     'adress':'Calle de Durango 186, Roma Nte., 06700 Ciudad de México, CDMX'
+   },
+
+  {'name':'Nuevo Jade',
+   'food':'arabe',
+   'photo':'..assets/images/huasteca.png',
+   'adress':'Av. Álvaro Obregón 264, Hipódromo Condesa, 06100 Ciudad de México, CDMX'
+ },
+
+  {'name':'Restaurant Ka-fú',
+   'food':'china',
+   'photo':'..assets/images/huasteca.png',
+   'adress':'Tlaxcala 134, Roma Sur, 06760 Cuauhtémoc, CDMX'
+ }
+]
